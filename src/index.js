a2fea274043592d489999109a97e20ab76b935c1
Mercy-mathitu <110403633+Mercy-mathitu@users.noreply.github.com>
2022-09-16 09:31:51 +0300

    Added JS to forecast

--- src/index.js
+++ src/index.js
@@ -21,6 +21,28 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML =`<div class="row">`;
+    let days = ["Fri", "Sat", "Sun", "Mon", "Tue"];
+    days.forEach(function(day) {
+    forecastHTML = forecastHTML + `
+        
+            <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" width="42">
+                <div class="weather-forecast-temperatures">
+                    <span class="weather-forecast-temperature-max">24°</span>
+                    <span class="weather-forecast-temperature-min">13°</span>
+                </div>
+            </div>
+        `;
+        });
+        forecastHTML = forecastHTML + `</div>`;
+        forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -83,4 +105,5 @@ let celsiusTemperature = null;
   celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 
-  search("Nairobi")
+  search("Nairobi");
+  displayForecast();
